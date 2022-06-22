ecf0e4e028b8b08a6142cfe66477ad0379e7d057
Colin Huber <cbhuber@gmail.com>
2022-06-21 19:56:21 -0600

    feat: Added sine curve best fit for violin data.

--- plot_er_wait_stats.py
+++ plot_er_wait_stats.py
@@ -3,6 +3,8 @@
 import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
 from capture_er_wait_data import DATE_TIME_FORMAT
 
 FONT_FAMILY = "Helvetica"
@@ -116,6 +118,65 @@ def plot_line(city, plot_offline=True, dark_mode=True):
 
 # -------------------------------------------------------------------------------------------------
 
+def create_hour_dict(hour, hour_dict):
+    """Creates a standard 12-hour format dictionary based on integer input.
+    :param: hour (int) hour between 0-23 inclusive
+    :param: hour_dict (dict) the dictionary to construct (e.g. 0 will be "12 AM")
+     :return: None"""
+
+    if hour < HALF_DAY_HOURS:
+        hour_dict[hour] = f'{hour} AM'
+        if hour % HALF_DAY_HOURS == 0:
+            hour_dict[hour] = f'{hour + HALF_DAY_HOURS} AM'
+    else:
+        hour_dict[hour] = f'{hour - HALF_DAY_HOURS} PM'
+        if hour % HALF_DAY_HOURS == 0:
+            hour_dict[hour] = f'{hour} PM'
+
+
+# -------------------------------------------------------------------------------------------------
+
+def get_sine_fit(df):
+    """Uses least squares to get a best-fit curve sine model.
+    :param: df (pd.DataFrame) Data frame containing the 24 hr data
+    :return: (list) y values representing the sine curve."""
+
+    # Get sinusoid best-fit as the median/mean avg of each hour
+    x_values = []
+    y_values = []
+
+    for hour in range(0, HOURS_IN_DAY):
+        x_values.append(hour)
+        y_values.append(float((df[hour].mean() + df[hour].median()) / 2.0))
+
+    x_values = np.array(x_values)
+    y_values = np.array(y_values)
+
+    guess_freq = 0
+    guess_amplitude = 3 * np.std(y_values) / (2 ** 0.5)
+    guess_phase = 0
+    guess_offset = np.mean(y_values)
+
+    p0 = [guess_freq, guess_amplitude, guess_phase, guess_offset]
+
+    def my_sine(x, freq, amplitude, phase, offset):
+        """Typical Sine function: amplitude * Sin(x*freq + phase) + offset
+        :param: x (int or float) x-coorindate
+        :param: freq (int or float) frequency (1 for 1 day)
+        :param: phase (int or float) phase offset
+        :param: offset (int or float) vertical offset
+        :return: amplitude * Sin(x*freq + phase) + offset"""
+        return np.sin(x * freq + phase) * amplitude + offset
+
+    curve_param, curve_covariance = curve_fit(my_sine, x_values, y_values, p0=p0)
+
+    sine_curve_fit = my_sine(x_values, *curve_param)
+
+    return sine_curve_fit
+
+
+# -------------------------------------------------------------------------------------------------
+
 def plot_violin(city, hospital, plot_offline=True, dark_mode=True):
     """Plots as violin data for each hour of the ER wait times.
     :param: city (str) City to be plotted
@@ -155,6 +216,10 @@ def plot_violin(city, hospital, plot_offline=True, dark_mode=True):
     # Filter by specific hospital
     df3 = df2[['time_stamp', hospital]].copy()
 
+    # Span of data for sub-title
+    min_date = min(df3['time_stamp'].dt.date)
+    max_date = max(df3['time_stamp'].dt.date)
+
     data = {}
     hour_dict = {}
 
@@ -162,23 +227,20 @@ def plot_violin(city, hospital, plot_offline=True, dark_mode=True):
     for hour in range(0, HOURS_IN_DAY):
         hour_filter = df3.time_stamp.dt.hour == hour
         data[hour] = df3[hour_filter][hospital].tolist()
-
-        # TODO: Put in a function
-        if hour < HALF_DAY_HOURS:
-            hour_dict[hour] = f'{hour} AM'
-            if hour % HALF_DAY_HOURS == 0:
-                hour_dict[hour] = f'{hour + HALF_DAY_HOURS} AM'
-        else:
-            hour_dict[hour] = f'{hour - HALF_DAY_HOURS} PM'
-            if hour % HALF_DAY_HOURS == 0:
-                hour_dict[hour] = f'{hour} PM'
+        create_hour_dict(hour, hour_dict)
 
     # Not all hours will have equal amount of data, create by day (cols) for every hour (rows) then transpose
     df4 = pd.DataFrame.from_dict(data, orient='index')
     df4 = df4.transpose()
 
+    sine_curve_fit = get_sine_fit(df4)
+
+    # Sin best-fit curve
+    plot_sine = go.Scatter(x=list(hour_dict.values()), y=sine_curve_fit, name="Average",
+                           line=dict(width=4, color='red'))
+
     layout = go.Layout(
-        title={'text': hospital + ' ER Wait Times',  # TODO: Put subtitle "Data spans from date X to date Y"
+        title={'text': hospital + f' ER Wait Times<br><sup>Date range: {min_date} to {max_date}</sup>',
                'x': 0.5,
                'y': 0.95,
                'xanchor': 'center',
@@ -195,7 +257,7 @@ def plot_violin(city, hospital, plot_offline=True, dark_mode=True):
         paper_bgcolor=color_mode['paper_bgcolor'][dark_mode],
         plot_bgcolor=color_mode['plot_bgcolor'][dark_mode],
         yaxis={'range': [0, 15]},
-        hoverdistance=100,
+        hoverdistance=50,
         hoverlabel=dict(
             font=dict(
                 size=16,
@@ -214,6 +276,8 @@ def plot_violin(city, hospital, plot_offline=True, dark_mode=True):
                                 name=hour_dict[hour],
                                 opacity=0.9))
 
+    fig.add_trace(plot_sine)
+
     if plot_offline:
         pyo.plot(fig, filename=html_file)
 
@@ -224,7 +288,13 @@ def plot_violin(city, hospital, plot_offline=True, dark_mode=True):
 
 
 if __name__ == "__main__":
-    plot_line("Calgary", False)
+    plot_line("Calgary", True)
     plot_line("Edmonton", False)
 
     plot_violin("Calgary", "South Health Campus")
+    # plot_violin("Calgary", "Alberta Children's Hospital")
+    # plot_violin("Calgary", "Foothills Medical Centre")
+    # plot_violin("Calgary", "Peter Lougheed Centre")
+    # plot_violin("Calgary", "Rockyview General Hospital")
+    # plot_violin("Calgary", "Sheldon M. Chumir Centre")
+    # plot_violin("Calgary", "South Calgary Health Centre")
